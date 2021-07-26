00e507babdf62c1b69e260d34712071c72900763
Jonas Horstmann <jonas_horstmann@avalance.ai>
2021-07-27 01:15:23 +0200

    implement error handling for mem module

---
+++ src/err.rs
@@ -0,0 +1,4 @@
+pub enum SysInfoError {}
+
+// general error struct (e.g. file handling etc)
+// every module has a custom error type, e.g. MemoryError
--- src/mem.rs
+++ src/mem.rs
@@ -2,8 +2,11 @@ use std::collections::{BTreeMap, HashMap};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{self, SeekFrom};
+use std::num;
 
-// tries to emulate behaviour of free(1) without having to call an external process everytime you want to check memory usage
+use thiserror::Error;
+
+/// tries to emulate behaviour of free(1) without having to call an external process everytime you want to check memory usage
 
 const MEM_PROC_FILE: &str = "/proc/meminfo";
 
@@ -11,6 +14,20 @@ const MEM_PROC_FILE: &str = "/proc/meminfo";
 const INITIAL_BUF_SIZE: usize = 2048;
 
 const INDEX_KEYS: [&str; 5] = ["MemTotal", "MemFree", "Buffers", "Cached", "SReclaimable"];
+
+#[derive(Error, Debug)]
+pub enum MemErr {
+    #[error("encountered unexpected format: {0}")]
+    UnexpectedFormat(&'static str),
+    #[error("key {0} is missing from {}", MEM_PROC_FILE)]
+    MissingValue(String),
+    #[error(transparent)]
+    FileHandlingError(#[from] io::Error),
+    #[error(transparent)]
+    ParseMemoryValue(#[from] num::ParseIntError),
+}
+
+type MemResult<T> = Result<T, MemErr>;
 
 pub struct MemInfo {
     file: File,
@@ -27,21 +44,29 @@ pub struct MemStats {
     used: usize,
 }
 
-fn build_line_index(file: &mut File, buf: &mut String) -> io::Result<BTreeMap<usize, String>> {
+fn build_lookup_table(file: &mut File, buf: &mut String) -> MemResult<BTreeMap<usize, String>> {
     file.read_to_string(buf)?;
     file.seek(SeekFrom::Start(0))?;
 
-    let keys: Vec<&str> = buf
-        .lines()
-        .map(|line: &str| line.split(":").nth(0).expect("wrong format"))
-        .collect();
+    let mut keys = Vec::new();
+    for line in buf.lines() {
+        let key = line
+            .split(':')
+            .nth(0)
+            .ok_or(MemErr::UnexpectedFormat("missing colon (:)"))?;
+
+        keys.push(key);
+    }
 
     let mut lookup = BTreeMap::new();
     for key in &INDEX_KEYS {
         if !keys.contains(key) {
-            // return custom error MISSING_KEY
+            return Err(MemErr::MissingValue(String::from(*key)));
         }
-        // should never panic because keys.contains() guards against it
+
+        // panic:
+        // every key should have a position in keys because
+        // this is the path where contains returned true
         let index = keys.iter().position(|s: &&str| *s == *key).unwrap();
         lookup.insert(index, String::from(*key));
     }
@@ -52,11 +77,11 @@ fn build_line_index(file: &mut File, buf: &mut String) -> io::Result<BTreeMap<us
 }
 
 impl MemInfo {
-    pub fn new() -> Result<Self, io::Error> {
+    pub fn new() -> MemResult<Self> {
         let mut file = File::open(MEM_PROC_FILE)?;
         let mut buf = String::with_capacity(INITIAL_BUF_SIZE);
 
-        let lookup = build_line_index(&mut file, &mut buf)?;
+        let lookup = build_lookup_table(&mut file, &mut buf)?;
 
         let mut cache = HashMap::with_capacity(lookup.len());
 
@@ -73,17 +98,24 @@ impl MemInfo {
         })
     }
 
-    fn parse_kb_value_from_line(&self, s: &str) -> usize {
+    fn parse_kb_value_from_line(&self, s: &str) -> MemResult<usize> {
         let end = s.len() - 3;
-        let start = s[..end].rfind(char::is_whitespace).unwrap() + 1;
+        let start = s
+            .get(..end)
+            .ok_or(MemErr::UnexpectedFormat("line is too short"))?
+            .rfind(char::is_whitespace)
+            .ok_or(MemErr::UnexpectedFormat(
+                "line doesn't contain any whitespace",
+            ))?
+            + 1;
 
-        s[start..end].parse().unwrap()
+        Ok(s[start..end].parse::<usize>()?)
     }
 
-    pub fn stats(&mut self) -> &MemStats {
-        self.file.read_to_string(&mut self.buf).unwrap();
+    pub fn stats(&mut self) -> MemResult<&MemStats> {
+        self.file.read_to_string(&mut self.buf)?;
         // set file position to 0 to read from start again next time stats is called
-        self.file.seek(SeekFrom::Start(0)).unwrap();
+        self.file.seek(SeekFrom::Start(0))?;
 
         let mut lines = self.buf.lines();
         let mut last_pos = 0;
@@ -94,10 +126,11 @@ impl MemInfo {
             let n = if advance == 0 { 0 } else { advance - 1 };
 
             // panic:
-            // the only way this panics is if the number of lines in /proc/meminfo changed between constructing MemInfo and calling this function
+            // the only way this panics is if the number of lines in /proc/meminfo
+            // changed between constructing MemInfo and calling this function,
             // which should never happen
             let line = lines.nth(n).unwrap();
-            let value = self.parse_kb_value_from_line(line);
+            let value = self.parse_kb_value_from_line(line)?;
 
             // panic:
             // new() made sure that all value names are already keys in this map
@@ -117,6 +150,6 @@ impl MemInfo {
         // clear keeps capacity but sets len to 0 to avoid appends next time stats is called
         self.buf.clear();
 
-        &self.stats
+        Ok(&self.stats)
     }
 }
